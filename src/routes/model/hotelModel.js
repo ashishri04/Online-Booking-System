2cf704f1fddf20c91fef95ad97e74ad97fb231a2
--replace-all <--add>
2023-05-22 13:51:45 +0530

    hotelSchema done

--- src/routes/model/hotelModel.js
+++ src/routes/model/hotelModel.js
@@ -1,1 +1,48 @@
-//name, address, hotel's email, description, ratings, facilities, room number, room type, availablity(T/F), 
+const mongoose = require("mongoose")
+
+const hotelSchema = new mongoose.Schema({
+    hotelName: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    address: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        unique: true,
+        required: true,
+        trim: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    rating: {
+        type: Number,
+        required: true
+    },
+    facilities: [{
+        type: String,
+        trim: true
+    }],
+    room_no: {
+        type: Number,
+        required: true
+    },
+    room_type: {
+        type: String,
+        enum: ["single", "double", "suite"],
+        required: true
+    },
+    availability: {
+        type: Boolean,
+        default: true
+    }
+}, { timestamps: true })
+
+
+module.export = mongoose.model("hotel", hotelSchema)
